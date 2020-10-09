d03d82081d39155b099079c3573d29c071c25c20
KaDiian <kadiidavidson@gmail.com>
2020-10-09 13:32:47 -0500

    Layout Board

---
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+  let squares = document.querySelectorAll('#board div');
+
+  for(let i = 0; i < squares.length; i++){
+    squares[i].classList.add('square');
+   
+    };
+  }
+
+
+
