1e140dddf05964240d42c1de3c92d0b23036c6bf
KaDiian <kadiidavidson@gmail.com>
2020-10-10 01:42:37 -0500

    Exercise 2

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,11 +1,27 @@
-window.onload = function(){
-  let squares = document.querySelectorAll('#board div');
-
-  for(let i = 0; i < squares.length; i++){
-    squares[i].classList.add('square');
-   
-    };
+window.onload = function() {
+  
+let squares = document.querySelectorAll('#board div');
+var x = 0 //Counts the amount of clicks
+var c = []
+ for(let i = 0; i < squares.length; i++) {
+   squares[i].classList.add('square');
+   squares[i].addEventListener('click', function(){
+      if(x%2==0) {
+        if(squares[i].innerHTML =='') {          
+          squares[i].innerHTML = 'X';
+          squares[i].classList.add ("X");
+          c.push(squares[i].innerHTML);                                    
+          x++;
+        }
+      }else
+      {
+        if(squares[i].innerHTML =='') {
+          squares[i].innerHTML = 'O';
+          squares[i].classList.add ("O");
+          c.push(squares[i].innerHTML);
+          x++;
+        }
+      }
+    });
   }
-
-
-
+}
