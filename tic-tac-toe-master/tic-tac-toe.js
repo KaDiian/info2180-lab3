ab99c4b23129db8e551a2779a3d8966ae2c093c3
KaDiian <kadiidavidson@gmail.com>
2020-10-10 19:36:05 -0500

    Exercise 3

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -3,25 +3,38 @@ window.onload = function() {
 let squares = document.querySelectorAll('#board div');
 var x = 0 //Counts the amount of clicks
 var c = []
- for(let i = 0; i < squares.length; i++) {
-   squares[i].classList.add('square');
-   squares[i].addEventListener('click', function(){
-      if(x%2==0) {
-        if(squares[i].innerHTML =='') {          
-          squares[i].innerHTML = 'X';
-          squares[i].classList.add ("X");
-          c.push(squares[i].innerHTML);                                    
-          x++;
-        }
-      }else
-      {
-        if(squares[i].innerHTML =='') {
-          squares[i].innerHTML = 'O';
-          squares[i].classList.add ("O");
-          c.push(squares[i].innerHTML);
-          x++;
-        }
-      }
-    });
-  }
+
+for(let i = 0; i < squares.length; i++){
+  squares[i].addEventListener('mouseover', function (){    
+    if(squares[i].innerHTML ==''){ 
+      squares[i].classList.add("hover")
+    }
+  });
+  
+  squares[i].addEventListener('mouseleave', function (){    
+      squares[i].classList.remove("hover");
+  });
 }
+
+for(let i = 0; i < squares.length; i++){
+  squares[i].classList.add('square');
+  squares[i].addEventListener('click', function(){
+    squares[i].classList.remove("hover");
+    if(x%2==0){
+      if(squares[i].innerHTML==''){
+        squares[i].classList.add ("square","X");
+        squares[i].innerHTML = 'X';
+        c.push(squares[i].innerHTML);                                    
+        x++;
+      }
+    }else
+    {
+      if(squares[i].innerHTML==''){
+        squares[i].classList.add ("square","O");
+        squares[i].innerHTML = 'O';
+        c.push(squares[i].innerHTML);
+        x++;
+      }
+    }
+  });
+}}
